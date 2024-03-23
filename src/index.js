dc7e9f0fec6594fc9db751b4fea94234a1011c62
Bpmodele <Bpmodele@users.noreply.github.com>
2024-03-23 15:26:34 -0400

    Changed AI generator from poem to sweets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-function displayPoem(response) {
-  new Typewriter("#poem", {
+function displayLocation(response) {
+  new Typewriter("#location", {
     strings: response.data.answer,
     autoStart: true,
     delay: 30,
@@ -7,23 +7,23 @@ function displayPoem(response) {
   });
 }
 
-function generatePoem(event) {
+function generateLocation(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "03cc42tob37268fffbcb4b84ee3110a3";
-  let prompt = `User instructions: Generate a Spanish poem about ${instructionsInput.value}`;
+  let prompt = `User instructions: Generate a description about ${instructionsInput.value}`;
   let context =
-    "You are a romanting poem expert and love to write short poems. Please generate a 4 line poem in basic HTML. Make sure to follow the user instructions. Do not inculde a title to the poem. Sign the poem with `SheCodes AI` inside a <strong> element at the end of the poem ";
+    "You are an expert in knowing the best sweets in different countries. Could you please generate a 4 line description in basic HTML. Make sure to follow the user instructions. Do not inculde a title to the poem. Sign the poem with `SheCodes AI` inside a <strong> element at the end of the poem ";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="blink">
-  ⌛Generating a Spanish poem about ${instructionsInput.value}</div>`;
+  let locationElement = document.querySelector("#location");
+  locationElement.classList.remove("hidden");
+  locationElement.innerHTML = `<div class="blink">
+  ⌛Generating a desription about ${instructionsInput.value} sweet</div>`;
 
-  axios.get(apiURL).then(displayPoem);
+  axios.get(apiURL).then(displayLocation);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+let locationFormElement = document.querySelector("#location-generator-form");
+locationFormElement.addEventListener("submit", generateLocation);
